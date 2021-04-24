d0b479fa50464594e469fe7f88862bf84651c499
Mateus B. Cassiano <mbc07@live.com>
2021-04-24 13:45:00 -0400

    Interface gráfica com Tkinter

--- flashbangbang.py
+++ flashbangbang.py
@@ -3,97 +3,165 @@ def somaRiemann(startX, endX, endY, startY, dataX, dataY, area, indexEqc):
     lenX = len(dataX)
     lenY = len(dataY)
 
-    eqc = ["(1 - x**2)**1/2", "x**2 + y**2", "e**(-x**2 - y**2 - z**2)"]
-
     if(indexEqc == 0):
         for x in range(startX, lenX-endX):
             for y in range(startY, lenY-endY):
-                resultado += ((1 - (dataX[x]**2))**0.5)*area 
+                resultado += ((1 - (dataX[x]**2))**0.5)*area
         return resultado
-    
+
     elif(indexEqc == 1):
         for x in range(startX, lenX-endX):
             for y in range(startY, lenY-endY):
-                resultado += ((dataX[x]**2) + (dataY[y]**2))*area 
+                resultado += ((dataX[x]**2) + (dataY[y]**2))*area
         return resultado
     else:
         return 1
 
-from PySimpleGUI import PySimpleGUI as sg
-sg.theme('Reddit')
 
-eqc = ["(1 - x**2)**1/2", "x**2 + y**2", "e**(-x**2 - y**2 - z**2)"]
+def calcula():
+    try:
+        indexEqc = lbxEquacao.curselection()[0]
 
-datas = [
-    [sg.T("Cantos Inferiores:",size=(15,1))],
-    [sg.T("   Esquerdo:", key="1",size=(15,1))],
-    [sg.T("   Direito:", key="2",size=(15,1))],
-    [sg.T("   Ponto Médio:", key="3",size=(16,1))],
-    [sg.T("Cantos Superiores:",size=(15,1))],
-    [sg.T("   Esquerdo:", key="4",size=(15,1))],
-    [sg.T("   Direito:", key="5",size=(15,1))]
-]
-
-ly = [
-    [sg.Listbox(eqc, size=(20,4), key='lista'), sg.Column(datas)],
-    [sg.Text("M ="), sg.In(size=(5,1), key="m"), sg.Text("N ="), sg.In(size=(5,1), key="n")],
-    [sg.Text("A ="), sg.In(size=(5,1), key="a"), sg.Text("B ="), sg.In(size=(5,1), key="b")],
-    [sg.Text("C ="), sg.In(size=(5,1), key="c"), sg.Text("D ="), sg.In(size=(5,1), key="d")],
-    [sg.Button("CALCULAR", key="calcular")]
-]
-
-wd = sg.Window('Soma de Riemann de Preguiçoso', ly, size=(400,400))
-
-ok = 1
-while ok:
-    event, values = wd.read()
-    if event == sg.WINDOW_CLOSED:
-        ok = 0
-
-    if event == 'calcular':
-        val = values['lista']
-
-        deltaX = (int(values['b']) - int(values['a']))/int(values['m'])
-        deltaY = (int(values['d']) - int(values['c']))/int(values['n'])
+        deltaX = (int(valB.get()) - int(valA.get()))/int(valM.get())
+        deltaY = (int(valD.get()) - int(valC.get()))/int(valN.get())
         area =   deltaX * deltaY
-        
-        xm = int(values['a'])
+
+        xm = int(valA.get())
         allX = []
-        for i in range(int(values['m']) + 1):
-            #print("{:.2f}".format(xm))
+        for i in range(int(valM.get()) + 1):
             allX.append(xm)
             xm += deltaX
 
-        yn = int(values['c'])
+        yn = int(valC.get())
         allY = []
-        for i in range(int(values['n']) + 1):
-            #print("{:.2f}".format(yn))
+        for i in range(int(valN.get()) + 1):
             allY.append(yn)
             yn += deltaY
-        
-        ciE = somaRiemann(0, 1, 0, 1, allX, allY, area, eqc.index(val[0]))
-        ciD = somaRiemann(1, 0, 0, 1, allX, allY, area, eqc.index(val[0]))
-        
-        #ponto medio
-        pM = 0
-        if eqc.index(val[0]) == 0:
+
+        ciE = somaRiemann(0, 1, 0, 1, allX, allY, area, indexEqc)
+        ciD = somaRiemann(1, 0, 0, 1, allX, allY, area, indexEqc)
+
+        # Ponto médio
+        pM = 0.0
+        if indexEqc == 0:
             for x in range(len(allX)-1):
                 for y in range(len(allY)-1):
                     pontoMedio = (allX[x] + allX[x+1])/2
                     pM += ((1 - (pontoMedio**2))**0.5)*area
 
-        elif eqc.index(val[0]) == 1:
+        elif indexEqc == 1:
             for x in range(len(allX)-1):
                 pontoMedioX = (allX[x] + allX[x+1])/2
                 for y in range(len(allY)-1):
                     pontoMedioY = (allY[y] + allY[y+1])/2
-                    pM += ((pontoMedioX**2) + (pontoMedioY**2))*area          
+                    pM += ((pontoMedioX**2) + (pontoMedioY**2))*area
 
-        csE = somaRiemann(0, 1, 0, 1, allX, allY, area, eqc.index(val[0]))
-        csD = somaRiemann(1, 0, 1, 0, allX, allY, area, eqc.index(val[0]))
-                
-        wd.Element("1").Update("  Esquerdo: {:.4f}".format(ciE))
-        wd.Element("2").Update("  Esquerdo: {:.4f}".format(ciD))
-        wd.Element("3").Update("Ponto Médio: {:.4f}".format(pM))
-        wd.Element("4").Update("  Esquerdo: {:.4f}".format(csE))
-        wd.Element("5").Update("  Direto: {:.4f}".format(csD))
+        csE = somaRiemann(0, 1, 0, 1, allX, allY, area, indexEqc)
+        csD = somaRiemann(1, 0, 1, 0, allX, allY, area, indexEqc)
+
+        valCIE.set("{:.6f}".format(float(ciE)))
+        valCID.set("{:.6f}".format(float(ciD)))
+        valCSE.set("{:.6f}".format(float(csE)))
+        valCSD.set("{:.6f}".format(float(csD)))
+        valPM.set("{:.6f}".format(float(pM)))
+
+    except:
+        valCIE.set("???")
+        valCID.set("???")
+        valCSE.set("???")
+        valCSD.set("???")
+        valPM.set("???")
+
+
+from tkinter import *
+from tkinter import ttk
+
+# Cria janela principal
+window = Tk()
+window.title("Soma de Riemann de Preguiçoso")
+window.columnconfigure(0, weight=1)
+window.rowconfigure(0, weight=1)
+frmMain = ttk.Frame(window, padding=(12,12,12,12))
+frmMain.grid(column=0, row=0, sticky=("N", "W", "S", "E"))
+
+
+# Painel de equações
+lbxEquacao = Listbox(frmMain, height=5, selectmode=SINGLE, activestyle="dotbox")
+lbxEquacao.grid(row=0, column=0, rowspan=4, columnspan=4, padx=6, pady=6, sticky=("N", "W", "S", "E"))
+lbxEquacao.insert(0, "(1 - x^2)^1/2")
+lbxEquacao.insert(1, "x^2 + y^2")
+lbxEquacao.insert(2, "e^(- x^2 - y^2 - z^2)")
+lbxEquacao.select_set(0)
+
+
+# Painel de entrada dos dados
+valM = StringVar(frmMain)
+valN = StringVar(frmMain)
+valA = StringVar(frmMain)
+valB = StringVar(frmMain)
+valC = StringVar(frmMain)
+valD = StringVar(frmMain)
+
+ttk.Label(frmMain, text="M =").grid(row=0, padx=6, column=4, sticky="E")
+ttk.Label(frmMain, text="N =").grid(row=0, padx=6, column=6, sticky="E")
+ttk.Label(frmMain, text="A =").grid(row=1, padx=6, column=4, sticky="E")
+ttk.Label(frmMain, text="B =").grid(row=1, padx=6, column=6, sticky="E")
+ttk.Label(frmMain, text="C =").grid(row=2, padx=6, column=4, sticky="E")
+ttk.Label(frmMain, text="D =").grid(row=2, padx=6, column=6, sticky="E")
+
+ttk.Entry(frmMain, width=10, textvariable=valM).grid(row=0, column=5, padx=0, pady=6, sticky="W")
+ttk.Entry(frmMain, width=10, textvariable=valN).grid(row=0, column=7, padx=6, pady=6, sticky="W")
+ttk.Entry(frmMain, width=10, textvariable=valA).grid(row=1, column=5, padx=0, pady=6, sticky="W")
+ttk.Entry(frmMain, width=10, textvariable=valB).grid(row=1, column=7, padx=6, pady=6, sticky="W")
+ttk.Entry(frmMain, width=10, textvariable=valC).grid(row=2, column=5, padx=0, pady=6, sticky="W")
+ttk.Entry(frmMain, width=10, textvariable=valD).grid(row=2, column=7, padx=6, pady=6, sticky="W")
+
+ttk.Button(frmMain, text="Calcular", command=calcula).grid(row=3, column=4, columnspan=4, padx=6, pady=6, sticky=("N", "W", "S", "E"))
+
+
+# Painel dos resultados
+valCIE = StringVar(frmMain)
+valCID = StringVar(frmMain)
+valCSE = StringVar(frmMain)
+valCSD = StringVar(frmMain)
+valPM  = StringVar(frmMain)
+
+lfrCI = ttk.Labelframe(frmMain, text="Cantos inferiores")
+lfrCS = ttk.Labelframe(frmMain, text="Cantos superiores")
+
+lfrCI.grid(row=4, column=0, rowspan=2, columnspan=4, padx=6, sticky=("N", "W", "S", "E"))
+lfrCS.grid(row=4, column=4, rowspan=2, columnspan=4, padx=6, sticky=("N", "W", "S", "E"))
+
+ttk.Label(lfrCI, text="  Esquerdo: ").grid(row=0, column=0, sticky="W")
+ttk.Label(lfrCI,  text="  Direito: ").grid(row=1, column=0, sticky="W")
+ttk.Label(lfrCI, textvariable=valCIE).grid(row=0, column=1, sticky="E")
+ttk.Label(lfrCI, textvariable=valCID).grid(row=1, column=1, sticky="E")
+
+ttk.Label(lfrCS, text="  Esquerdo: ").grid(row=0, column=0, sticky="W")
+ttk.Label(lfrCS,  text="  Direito: ").grid(row=1, column=0, sticky="W")
+ttk.Label(lfrCS, textvariable=valCSE).grid(row=0, column=1, sticky="E")
+ttk.Label(lfrCS, textvariable=valCSD).grid(row=1, column=1, sticky="E")
+
+ttk.Label(frmMain, text="  Ponto médio:").grid(row=6, column=0, sticky="W")
+ttk.Label(frmMain,    textvariable=valPM).grid(row=6, column=0, sticky="E")
+
+
+# Configura redimensionamento da janela principal
+frmMain.columnconfigure(0, weight=4)
+frmMain.columnconfigure(4, weight=1)
+frmMain.columnconfigure(5, weight=1)
+frmMain.columnconfigure(6, weight=1)
+frmMain.columnconfigure(7, weight=1)
+
+frmMain.rowconfigure(0, weight=1)
+frmMain.rowconfigure(1, weight=1)
+frmMain.rowconfigure(2, weight=1)
+frmMain.rowconfigure(3, weight=0)
+frmMain.rowconfigure(4, weight=0)
+frmMain.rowconfigure(5, weight=0)
+
+window.minsize(window.winfo_reqwidth()+320,window.winfo_reqheight()+90)
+
+
+# Exibe a interface
+window.mainloop()
