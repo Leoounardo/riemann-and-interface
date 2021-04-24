c5e4f35f591a42b27ceff6dfe94fb6abb2cba0f4
Mateus B. Cassiano <mbc07@live.com>
2021-04-24 18:33:18 -0400

    Implementa cálculo do exercício 4

--- flashbangbang.py
+++ flashbangbang.py
@@ -1,4 +1,6 @@
-def somaRiemann(startX, endX, endY, startY, dataX, dataY, area, indexEqc):
+import math
+
+def somaRiemann2(startX, endX, startY, endY, dataX, dataY, area, indexEqc):
     resultado = 0
     lenX = len(dataX)
     lenY = len(dataY)
@@ -7,16 +9,26 @@ def somaRiemann(startX, endX, endY, startY, dataX, dataY, area, indexEqc):
         for x in range(startX, lenX-endX):
             for y in range(startY, lenY-endY):
                 resultado += ((1 - (dataX[x]**2))**0.5)*area
-        return resultado
 
     elif(indexEqc == 1):
         for x in range(startX, lenX-endX):
             for y in range(startY, lenY-endY):
                 resultado += ((dataX[x]**2) + (dataY[y]**2))*area
-        return resultado
-    else:
-        return 1
 
+    return resultado
+
+def somaRiemann3(startX, endX, startY, endY, startZ, endZ, dataX, dataY, dataZ, area):
+    resultado = 0
+    lenX = len(dataX)
+    lenY = len(dataY)
+    lenZ = len(dataZ)
+
+    for x in range(startX, lenX-endX):
+        for y in range(startY, lenY-endY):
+            for z in range(startZ, lenZ-endZ):
+                resultado += (math.e**(-(dataX[x]**2) - (dataY[y]**2) - (dataZ[z]**2)))*area
+
+    return resultado
 
 def calcula():
     try:
@@ -24,7 +36,12 @@ def calcula():
 
         deltaX = (int(valB.get()) - int(valA.get()))/int(valM.get())
         deltaY = (int(valD.get()) - int(valC.get()))/int(valN.get())
-        area =   deltaX * deltaY
+
+        if(indexEqc == 2):
+            deltaZ = (int(valF.get()) - int(valE.get()))/int(valO.get())
+            area = deltaX * deltaY * deltaZ
+        else:
+            area =   deltaX * deltaY
 
         xm = int(valA.get())
         allX = []
@@ -38,32 +55,55 @@ def calcula():
             allY.append(yn)
             yn += deltaY
 
-        ciE = somaRiemann(0, 1, 0, 1, allX, allY, area, indexEqc)
-        ciD = somaRiemann(1, 0, 0, 1, allX, allY, area, indexEqc)
+        if(indexEqc == 2):
+            zo = int(valE.get())
+            allZ = []
+            for i in range(int(valO.get()) + 1):
+                allZ.append(zo)
+                zo += deltaZ
+
+            ciE = somaRiemann3(0, 1, 1, 0, 0, 1, allX, allY, allZ, area)
+            ciD = somaRiemann3(1, 0, 1, 0, 1, 0, allX, allY, allZ, area)
+        else:
+            ciE = somaRiemann2(0, 1, 1, 0, allX, allY, area, indexEqc)
+            ciD = somaRiemann2(1, 0, 1, 0, allX, allY, area, indexEqc)
 
         # Ponto médio
         pM = 0.0
-        if indexEqc == 0:
+        if(indexEqc == 0):
             for x in range(len(allX)-1):
                 for y in range(len(allY)-1):
                     pontoMedio = (allX[x] + allX[x+1])/2
                     pM += ((1 - (pontoMedio**2))**0.5)*area
 
-        elif indexEqc == 1:
+        elif(indexEqc == 1):
             for x in range(len(allX)-1):
                 pontoMedioX = (allX[x] + allX[x+1])/2
                 for y in range(len(allY)-1):
                     pontoMedioY = (allY[y] + allY[y+1])/2
                     pM += ((pontoMedioX**2) + (pontoMedioY**2))*area
 
-        csE = somaRiemann(0, 1, 0, 1, allX, allY, area, indexEqc)
-        csD = somaRiemann(1, 0, 1, 0, allX, allY, area, indexEqc)
+        elif(indexEqc == 2):
+            for x in range(len(allX)-1):
+                pontoMedioX = (allX[x] + allX[x+1])/2
+                for y in range(len(allY)-1):
+                    pontoMedioY = (allY[y] + allY[y+1])/2
+                    for z in range(len(allZ)-1):
+                        pontoMedioZ = (allZ[z] + allZ[z+1])/2
+                        pM += (math.e**(-(pontoMedioX**2) - (pontoMedioY**2) - (pontoMedioZ**2)))*area
 
-        valCIE.set("{:.6f}".format(float(ciE)))
-        valCID.set("{:.6f}".format(float(ciD)))
-        valCSE.set("{:.6f}".format(float(csE)))
-        valCSD.set("{:.6f}".format(float(csD)))
-        valPM.set("{:.6f}".format(float(pM)))
+        if(indexEqc == 2):
+            csE = somaRiemann3(0, 1, 1, 0, 0, 1, allX, allY, allZ, area)
+            csD = somaRiemann3(1, 0, 0, 1, 0, 1, allX, allY, allZ, area)
+        else:
+            csE = somaRiemann2(0, 1, 1, 0, allX, allY, area, indexEqc)
+            csD = somaRiemann2(1, 0, 0, 1, allX, allY, area, indexEqc)
+
+        valCIE.set("{:.4f}".format(float(ciE)))
+        valCID.set("{:.4f}".format(float(ciD)))
+        valCSE.set("{:.4f}".format(float(csE)))
+        valCSD.set("{:.4f}".format(float(csD)))
+        valPM.set("{:.4f}".format(float(pM)))
 
     except:
         valCIE.set("???")
@@ -72,6 +112,18 @@ def calcula():
         valCSD.set("???")
         valPM.set("???")
 
+def atualizaEntradas(*args):
+    try:
+        if(lbxEquacao.curselection()[0] == 2):
+            entO["state"]="normal"
+            entE["state"]="normal"
+            entF["state"]="normal"
+        else:
+            entO["state"]="disabled"
+            entE["state"]="disabled"
+            entF["state"]="disabled"
+    except:
+        pass
 
 from tkinter import *
 from tkinter import ttk
@@ -84,40 +136,52 @@ window.rowconfigure(0, weight=1)
 frmMain = ttk.Frame(window, padding=(12,12,12,12))
 frmMain.grid(column=0, row=0, sticky=("N", "W", "S", "E"))
 
-
 # Painel de equações
 lbxEquacao = Listbox(frmMain, height=5, selectmode=SINGLE, activestyle="dotbox")
 lbxEquacao.grid(row=0, column=0, rowspan=4, columnspan=4, padx=6, pady=6, sticky=("N", "W", "S", "E"))
 lbxEquacao.insert(0, "(1 - x^2)^1/2")
 lbxEquacao.insert(1, "x^2 + y^2")
 lbxEquacao.insert(2, "e^(- x^2 - y^2 - z^2)")
+lbxEquacao.bind('<<ListboxSelect>>', atualizaEntradas)
 lbxEquacao.select_set(0)
-
 
 # Painel de entrada dos dados
 valM = StringVar(frmMain)
 valN = StringVar(frmMain)
+valO = StringVar(frmMain)
 valA = StringVar(frmMain)
 valB = StringVar(frmMain)
 valC = StringVar(frmMain)
 valD = StringVar(frmMain)
+valE = StringVar(frmMain)
+valF = StringVar(frmMain)
 
-ttk.Label(frmMain, text="M =").grid(row=0, padx=6, column=4, sticky="E")
-ttk.Label(frmMain, text="N =").grid(row=0, padx=6, column=6, sticky="E")
-ttk.Label(frmMain, text="A =").grid(row=1, padx=6, column=4, sticky="E")
-ttk.Label(frmMain, text="B =").grid(row=1, padx=6, column=6, sticky="E")
-ttk.Label(frmMain, text="C =").grid(row=2, padx=6, column=4, sticky="E")
-ttk.Label(frmMain, text="D =").grid(row=2, padx=6, column=6, sticky="E")
+ttk.Label(frmMain, text="M =").grid(row=0, column=4, padx=6, sticky="E")
+ttk.Label(frmMain, text="N =").grid(row=0, column=6, padx=6, sticky="E")
+ttk.Label(frmMain, text="O =").grid(row=0, column=8, padx=6, sticky="E")
+ttk.Label(frmMain, text="A =").grid(row=1, column=4, padx=6, sticky="E")
+ttk.Label(frmMain, text="B =").grid(row=2, column=4, padx=6, sticky="E")
+ttk.Label(frmMain, text="C =").grid(row=1, column=6, padx=6, sticky="E")
+ttk.Label(frmMain, text="D =").grid(row=2, column=6, padx=6, sticky="E")
+ttk.Label(frmMain, text="E =").grid(row=1, column=8, padx=6, sticky="E")
+ttk.Label(frmMain, text="F =").grid(row=2, column=8, padx=6, sticky="E")
 
-ttk.Entry(frmMain, width=10, textvariable=valM).grid(row=0, column=5, padx=0, pady=6, sticky="W")
-ttk.Entry(frmMain, width=10, textvariable=valN).grid(row=0, column=7, padx=6, pady=6, sticky="W")
-ttk.Entry(frmMain, width=10, textvariable=valA).grid(row=1, column=5, padx=0, pady=6, sticky="W")
-ttk.Entry(frmMain, width=10, textvariable=valB).grid(row=1, column=7, padx=6, pady=6, sticky="W")
-ttk.Entry(frmMain, width=10, textvariable=valC).grid(row=2, column=5, padx=0, pady=6, sticky="W")
-ttk.Entry(frmMain, width=10, textvariable=valD).grid(row=2, column=7, padx=6, pady=6, sticky="W")
+ttk.Entry(frmMain, width=5, textvariable=valM).grid(row=0, column=5, padx=6, pady=6, sticky="W")
+ttk.Entry(frmMain, width=5, textvariable=valN).grid(row=0, column=7, padx=0, pady=6, sticky="W")
+ttk.Entry(frmMain, width=5, textvariable=valA).grid(row=1, column=5, padx=6, pady=6, sticky="W")
+ttk.Entry(frmMain, width=5, textvariable=valB).grid(row=2, column=5, padx=6, pady=6, sticky="W")
+ttk.Entry(frmMain, width=5, textvariable=valC).grid(row=1, column=7, padx=0, pady=6, sticky="W")
+ttk.Entry(frmMain, width=5, textvariable=valD).grid(row=2, column=7, padx=0, pady=6, sticky="W")
 
-ttk.Button(frmMain, text="Calcular", command=calcula).grid(row=3, column=4, columnspan=4, padx=6, pady=6, sticky=("N", "W", "S", "E"))
+entO = ttk.Entry(frmMain, width=5, textvariable=valO, state="disabled")
+entE = ttk.Entry(frmMain, width=5, textvariable=valE, state="disabled")
+entF = ttk.Entry(frmMain, width=5, textvariable=valF, state="disabled")
 
+entO.grid(row=0, column=9, padx=6, pady=6, sticky="W")
+entE.grid(row=1, column=9, padx=6, pady=6, sticky="W")
+entF.grid(row=2, column=9, padx=6, pady=6, sticky="W")
+
+ttk.Button(frmMain, text="Calcular", command=calcula).grid(row=3, column=4, columnspan=6, padx=6, pady=6, sticky=("N", "W", "S", "E"))
 
 # Painel dos resultados
 valCIE = StringVar(frmMain)
@@ -129,8 +193,8 @@ valPM  = StringVar(frmMain)
 lfrCI = ttk.Labelframe(frmMain, text="Cantos inferiores")
 lfrCS = ttk.Labelframe(frmMain, text="Cantos superiores")
 
-lfrCI.grid(row=4, column=0, rowspan=2, columnspan=4, padx=6, sticky=("N", "W", "S", "E"))
-lfrCS.grid(row=4, column=4, rowspan=2, columnspan=4, padx=6, sticky=("N", "W", "S", "E"))
+lfrCI.grid(row=4, column=0, rowspan=2, columnspan=5, padx=6, sticky=("N", "W", "S", "E"))
+lfrCS.grid(row=4, column=5, rowspan=2, columnspan=5, padx=6, sticky=("N", "W", "S", "E"))
 
 ttk.Label(lfrCI, text="  Esquerdo: ").grid(row=0, column=0, sticky="W")
 ttk.Label(lfrCI,  text="  Direito: ").grid(row=1, column=0, sticky="W")
@@ -143,15 +207,16 @@ ttk.Label(lfrCS, textvariable=valCSE).grid(row=0, column=1, sticky="E")
 ttk.Label(lfrCS, textvariable=valCSD).grid(row=1, column=1, sticky="E")
 
 ttk.Label(frmMain, text="  Ponto médio:").grid(row=6, column=0, sticky="W")
-ttk.Label(frmMain,    textvariable=valPM).grid(row=6, column=0, sticky="E")
-
+ttk.Label(frmMain,    textvariable=valPM).grid(row=6, column=1, sticky="E")
 
 # Configura redimensionamento da janela principal
-frmMain.columnconfigure(0, weight=4)
+frmMain.columnconfigure(0, weight=6)
 frmMain.columnconfigure(4, weight=1)
 frmMain.columnconfigure(5, weight=1)
 frmMain.columnconfigure(6, weight=1)
 frmMain.columnconfigure(7, weight=1)
+frmMain.columnconfigure(8, weight=1)
+frmMain.columnconfigure(9, weight=1)
 
 frmMain.rowconfigure(0, weight=1)
 frmMain.rowconfigure(1, weight=1)
@@ -161,7 +226,6 @@ frmMain.rowconfigure(4, weight=0)
 frmMain.rowconfigure(5, weight=0)
 
 window.minsize(window.winfo_reqwidth()+320,window.winfo_reqheight()+90)
-
 
 # Exibe a interface
 window.mainloop()
